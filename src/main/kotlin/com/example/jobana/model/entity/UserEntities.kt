b5ac2f5eed7599819608838403381ad2c385ea1c
Андрей Левченко <ComradeAndrew111119@yandex.ru>
2022-12-14 15:24:37 +0300

    Added CV entity, changed user entity

--- src/main/kotlin/com/example/jobana/model/entity/UserEntities.kt
+++ src/main/kotlin/com/example/jobana/model/entity/UserEntities.kt
@@ -1,13 +1,12 @@
 package com.example.jobana.model.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.EnumType
-import jakarta.persistence.Enumerated
+import jakarta.persistence.*
+import org.intellij.lang.annotations.Pattern
 import java.time.LocalDate
 
 @Entity
-class UserEntity(
+@Table(name = "Users")
+class User(
         @Column(nullable = false, length = 128)
         var firstName : String,
 
@@ -26,7 +25,30 @@ class UserEntity(
 
 
         @Column(unique = true, nullable = false, length = 320)
-        var email : String
+        @Pattern(value = "")
+        var email : String,
 
+        @OneToOne
+        @JoinColumn(name="cv_id", referencedColumnName = "id")
+        var cv : CV
+
+
+) : AbstractEntity()
+
+
+@Entity
+@Table(name = "CV")
+class CV(
+        @OneToOne(mappedBy = "cv")
+        var user: User,
+
+        @Column(length = 1024)
+        var hardSkills : String,
+
+        @Column(length = 1024)
+        var softSkills : String,
+
+        @Column(length = 1024)
+        var aboutMyself : String,
 
 ) : AbstractEntity()
